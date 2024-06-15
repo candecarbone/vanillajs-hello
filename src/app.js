b5feaa7a05e4874d31cc6c06c90065883e2c959a
candeecarbone <103152898+candecarbone@users.noreply.github.com>
2024-06-15 09:39:13 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function excuseGenerator() {
+  let who = ["My cat", "My cousin", "My fish", "My grandma", "My neighbor"];
+  let action = ["ate", "hid", "crushed", "robbed"];
+  let what = ["my homework", "my phone", "the car", "my bike", "my lunch"];
+  let when = [
+    "before the class started",
+    "when I was sleeping",
+    "while I was at church",
+    "during my free period",
+    "while I was away"
+  ];
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = excuseGenerator();
 };
